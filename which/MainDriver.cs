a0d2edd668cc69010a01b77fb25704b8e7f505ae
qua <quamv@outlook.com>
2018-09-13 14:05:47 -0400

    version 1

---
+++ which/MainDriver.cs
@@ -0,0 +1,44 @@
+﻿using DavisSquareUtils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DavisSquareCmdUtils
+{
+    class MainDriver
+    {
+        static void Usage()
+        {
+            throw new Exception("which [-p] command");
+        }
+
+        static void Main(string[] args)
+        {
+            string command = null;
+            bool show_paths = false;
+
+            for (var i = 0; i < args.Length; i++)
+            {
+                if (args[i][0] == '-')
+                {
+                    switch (args[i][1])
+                    {
+                        case 'p': show_paths = true; break;
+                        default: throw new Exception("Illegal argument: " + args[i]);
+                    }
+                }
+                else
+                {
+                    if (command != null)
+                        Usage();
+
+                    command = args[i];
+                }
+            }
+
+            which.Resolve(command, show_paths);
+        }
+    }
+}
--- which/Program.cs
+++ which/Program.cs
@@ -5,45 +5,108 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace which
+namespace DavisSquareUtils
 {
-    class Program
+    class which
     {
-        static void Main(string[] args)
+        public static void Resolve(string command_name, bool show_paths)
         {
-            if (args.Length != 1)
-                throw new Exception("Usage: which <command>");
+            /* 
+             * if the command name includes a '.', assume full extension is specified and only match on the 
+             * exact match (e.g. "notepad.exe")
+             * otherwise, check for any eligible matches based on the file extensions in pathext_extensions
+             */
+
+            if (command_name.Contains("."))
+                exact_match(command_name, show_paths);
+            else
+                fuzzy_match(command_name);
+        }
+
+        static IEnumerable<string> get_path_dirs()
+            => Environment.GetEnvironmentVariable("PATH").Split(';');
+
+        static void success(string filepath)
+            => Console.WriteLine(filepath);
+
+        static void fail(string command_name)
+            => Console.WriteLine("Command: " + command_name + " not found in path.");
 
 
-            System.Text.StringBuilder str = new StringBuilder();
-            var path_dirs = Environment.GetEnvironmentVariable("PATH").Split(';');
-            var user_arg = args[0];
-            var search_string = (user_arg.Contains(".") ? user_arg : "*" + user_arg + "*");
-            bool error_occurred = false;
-            string live_indicator = " <-- Active";
+        /* user entered command with extension. only match on exact match. 
+         * (as opposed to checking <command>.exe, <command>.com, etc...
+         */
+        static void exact_match(string command_name, bool show_paths)
+        {
+            foreach (var dir in get_path_dirs())
+            {
+                if (show_paths)
+                    Console.WriteLine("Searching " + dir);
+
+                try
+                {
+                    var matching_file = new DirectoryInfo(dir)
+                        .GetFiles(command_name)
+                        .FirstOrDefault();
+
+                    if (matching_file != null)
+                    {
+                        success(matching_file.FullName);
+                        return;
+                    }
+                }
+                catch (Exception ex) { err(ex); }
+            }
+
+            fail(command_name);
+        }
+
+        static void err(Exception ex)
+            => Console.WriteLine(ex.InnerException == null ? ex.Message : ex.Message + "\n" + ex.InnerException.Message + "\n");
+
+        static string [] get_extensions ()
+        {
+            /* if present, pathext environment variable determines suffix search order 
+             * otherwise, use defaults from here 
+             * https://stackoverflow.com/questions/605101/order-in-which-command-prompt-executes-files-with-the-same-name-a-bat-vs-a-cmd
+             */
+            var pathext_environment_variable =
+                Environment.GetEnvironmentVariable("PATHEXT");
+
+            return (pathext_environment_variable != null)
+                    ? pathext_environment_variable.Split(';')
+                    : new string[] { ".com", ".exe", ".bat", ".cmd" }; 
+        }
+
+        static void fuzzy_match(string command_name)
+        {
+            var path_dirs =
+                get_path_dirs();
+
+            var path_extensions =
+                get_extensions();
+
             foreach (var dir in path_dirs)
             {
                 try
                 {
-                    var files = new DirectoryInfo(dir).GetFiles(search_string);
+                    var loose_matches = 
+                        new DirectoryInfo(dir).GetFiles(command_name + ".*");
 
-                    foreach (FileInfo file in files)
+                    foreach (var ext in path_extensions)
                     {
-                        str = str.AppendLine(file.FullName + live_indicator);
-                        live_indicator = "";
+                        var exact_match = 
+                            loose_matches.SingleOrDefault(f => f.Name.ToLowerInvariant() == (command_name + ext).ToLowerInvariant());
+
+                        if (exact_match != null)
+                        {
+                            success(exact_match.FullName);
+                            return;
+                        }
                     }
                 }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.InnerException == null ? ex.Message : ex.Message + "\n" + ex.InnerException.Message);
-                    error_occurred = true;
-                }
-
+                catch (Exception ex) { err(ex); }
             }
-            if (error_occurred)
-                Console.WriteLine("\n");
-            Console.WriteLine("\n------\n");
-            Console.WriteLine(str.ToString());
         }
     }
 }
