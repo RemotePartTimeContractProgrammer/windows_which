beb2179d52c66751b555602ce307e864eb7e4629
qua <quamv@outlook.com>
2018-09-13 15:37:04 -0400

    consolidate into single file

--- which/Program.cs
+++ which/Program.cs
@@ -105,5 +105,35 @@ namespace CmdUtils
                 catch (Exception ex) { err(ex); }
             }
         }
+
+        static void Main(string[] args)
+        {
+            string command = null;
+            bool show_paths = false;
+
+            for (var i = 0; i < args.Length; i++)
+            {
+                if (args[i][0] == '-')
+                {
+                    switch (args[i][1])
+                    {
+                        case 'p':
+                            show_paths = true;
+                            break;
+                        default:
+                            throw new Exception("Illegal argument: " + args[i]);
+                    }
+                }
+                else
+                {
+                    if (command != null)
+                        throw new Exception("which [-p] command");
+
+                    command = args[i];
+                }
+            }
+
+            which.Resolve(command, show_paths);
+        }
     }
 }
