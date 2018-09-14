a14cb211e9184ee899c473b4462d45a24ede5755
qua <quamv@outlook.com>
2018-09-14 17:02:58 -0400

    added simplest c# implementation

---
+++ simplest/which.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CmdUtils
+{
+    class which
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("no command specified");
+                return;
+            }
+
+            string command = args[0];
+
+            var path_dirs = Environment.GetEnvironmentVariable("PATH")
+                .Split(';')
+                .Where(dir => dir != null && dir != "")
+                .ToList();
+
+            var first = path_dirs
+                .FirstOrDefault(dir => System.IO.File.Exists(dir + "\\" + command));
+
+            Console.WriteLine(
+                (first != null)
+                ? "command found at " + first + "\\" + command
+                : "command not found");
+        }
+    }
+}
